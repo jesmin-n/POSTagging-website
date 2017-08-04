6dfae50e46c6ed8bc3b8a16b7344f2e87b34b67a
Jesmin Ngo <xjesminngo@berkeley.edu>
2017-08-04 02:09:32 -0700

    basic functionality to input text and get: - nouns - adjectives - cardinal numbers - verbs

---
+++ logic.js
@@ -0,0 +1,11 @@
+// var words = new Lexer().lex(document.getElementById("input-text").innerHTML);
+// var taggedWords = new POSTagger().tag(words);
+// var result = "";
+// for (i in taggedWords) {
+//   var taggedWord = taggedWords[i];
+//   var word = taggedWord[0];
+//   var tag = taggedWord[1];
+//   // Note the use of document.writeln instead of print
+//   result += (word + " /" + tag + "<br/>");
+// }
+// console.log(result);
